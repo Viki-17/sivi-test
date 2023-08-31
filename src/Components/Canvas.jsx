8258e3b6fd0da34bccfb246fda57e806ef56412d
Vignesh Raviandar <vignesh@Vigneshs-MacBook-Air.local>
2023-08-31 23:04:37 +0530

    created a constant boxes array which defines the width, height and color of boxes from which mapped those boxes and added listeners

--- src/Components/Canvas.jsx
+++ src/Components/Canvas.jsx
@@ -1,68 +1,105 @@
 import React, { useEffect, useState } from "react";
 import "./Canvas.css";
-function Canvas() {
-  const [recPos, SetRecPos] = useState({});
+
+const BOXES = [
+  {
+    id: 1,
+    width: "110px",
+    height: "105px",
+    backgroundColor: "yellow",
+  },
+  {
+    id: 2,
+    width: "100px",
+    height: "150px",
+    backgroundColor: "blue",
+  },
+];
+
+function dragBox(e, rec, setPositions, id) {
   let pos1 = 0,
     pos2 = 0,
     pos3 = 0,
     pos4 = 0;
-  let recClass = {
-    position: "absolute",
-    border: "2px solid green",
-    width: "80px",
-    height: "100px",
-    left: recPos.left?.left + "px",
-    top: recPos.top?.top + "px",
-  };
 
-  useEffect(() => {
-    let rec = document.getElementById("rec2");
+  //   mouseDrag(e);
+  //   function mouseDrag(e) {
+  e.preventDefault();
 
-    rec.addEventListener("mousedown", mouseDrag);
-    console.log(rec.offsetTop);
-    function mouseDrag(e) {
-      e.preventDefault();
+  pos3 = e.clientX;
+  pos4 = e.clientY;
+  document.onmouseup = closeDrag;
+  document.onmousemove = elementDrag;
+  //   }
 
-      pos3 = e.clientX;
-      pos4 = e.clientY;
-      document.onmouseup = closeDrag;
-      document.onmousemove = elementDrag;
-    }
+  function elementDrag(e) {
+    e.preventDefault();
 
-    function elementDrag(e) {
-      e.preventDefault();
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
 
-      pos1 = pos3 - e.clientX;
-      pos2 = pos4 - e.clientY;
-      pos3 = e.clientX;
-      pos4 = e.clientY;
-      SetRecPos((prev) => ({
-        left: {
-          ...prev.left,
+    setPositions((prev) => {
+      const pos = {
+        ...prev,
+        [id]: {
           left: rec.offsetLeft - pos1,
-        },
-        top: {
-          ...prev.top,
           top: rec.offsetTop - pos2,
         },
-      }));
-    }
+      };
+      return pos;
+    });
+  }
 
-    function closeDrag() {
-      document.onmouseup = null;
-      document.onmousemove = null;
-    }
+  function closeDrag() {
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
+function Canvas({ boxes = BOXES }) {
+  const [positions, setPositions] = useState({});
+
+  useEffect(() => {
+    boxes.forEach((box) => {
+      const rec = document.getElementById(`box-${box.id}`);
+
+      rec.addEventListener("mousedown", (e) => {
+        dragBox(e, rec, setPositions, box.id);
+      });
+    });
   }, []);
 
-  console.log(recPos.left?.left, recPos.top?.top);
   return (
     <>
       <div className="canvas">
-        <div className="rec"></div>
-        <div style={recClass} id="rec2"></div>
+        {/* <div className="rec"></div> */}
+        {boxes.map((box) => {
+          return (
+            <div
+              id={`box-${box.id}`}
+              key={box.id}
+              style={getBoxStyles(box, positions)}
+            />
+          );
+        })}
       </div>
     </>
   );
+}
+function getBoxStyles(box, positions) {
+  const styles = {
+    position: "absolute",
+    height: box.height,
+    width: box.width,
+    backgroundColor: box.backgroundColor,
+    left: (positions[box.id]?.left || 0) + "px",
+    top: (positions[box.id]?.top || 0) + "px",
+  };
+  console.log({ styles });
+
+  return styles;
 }
 
 export default Canvas;
